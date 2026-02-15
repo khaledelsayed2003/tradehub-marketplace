fc2734cf853cb40d46ceee27acea42bebfd4e6b4
Khaled Elsayed <khaled.elsayed2206@gmail.com>
2026-02-15 18:55:41 +0300

    feat(auth): create and save user into db after successful registration form submit

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
-from app import app
-from flask import render_template
-from app.models import Item
+from app import app, db
+from flask import render_template, redirect, url_for
+from app.models import Item, User
 from app.forms import RegisterForm
 
 @app.route("/")
@@ -13,7 +13,12 @@ def market_page():
     items = Item.query.all()
     return render_template('market.html', items=items)
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+        created_user = User(username=form.username.data, email_address=form.email_address.data, password_hash=form.password.data)
+        db.session.add(created_user)
+        db.session.commit()
+        return redirect(url_for('market_page'))
     return render_template('register.html', form=form)
